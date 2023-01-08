0fc76cf8dd318b6e944ca8054623fc0eec049d55
BKD471 <bhaskarkumardas9@gmail.com>
2023-01-08 22:35:40 +0530

    Adding Enttity,DTO,Controller,Repository  for Comments & adding create comment Rest service

--- src/main/java/com/example/Spring/Boot/Blog/service/impl/CommentServiceImpl.java
+++ src/main/java/com/example/Spring/Boot/Blog/service/impl/CommentServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.Spring.Boot.Blog.service.impl;
 
 import com.example.Spring.Boot.Blog.dto.CommentDto;
+import com.example.Spring.Boot.Blog.exception.ResourceNotFoundException;
 import com.example.Spring.Boot.Blog.model.Comments;
 import com.example.Spring.Boot.Blog.model.Post;
 import com.example.Spring.Boot.Blog.repository.CommentRepository;
@@ -15,42 +16,42 @@ public class CommentServiceImpl implements CommentService {
 
     private final CommentRepository commentRepository;
     private final PostRepository postRepository;
-    public CommentServiceImpl(CommentRepository commentRepository,PostRepository postRepository){
-        this.commentRepository=commentRepository;
-        this.postRepository=postRepository;
+
+    public CommentServiceImpl(CommentRepository commentRepository, PostRepository postRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
     }
+
     @Override
-    public CommentDto createComment(CommentDto commentDto, long id){
+    public CommentDto createComment(CommentDto commentDto, long id) {
         // Convert DTo to entity
-        Comments comments=mapToEntity(commentDto);
+        Comments comments = mapToEntity(commentDto);
         //Fetching Post by id
-        Optional<Post> post=postRepository.findById(id);
+        Post fetchedPost = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("post", "id", id));
 
-        if(post.isPresent()){
-            Post fetchedPost=post.get();
-            comments.setPost(fetchedPost);
-            //save comments to repository
-            Comments savedComment=commentRepository.save(comments);
-            //map to Dto
-            return mapToDto(savedComment);
-        }
-        return null;
+        comments.setPost(fetchedPost);
+        //save comments to repository
+        Comments savedComment = commentRepository.save(comments);
+        //map to Dto
+        return mapToDto(savedComment);
+
     }
 
-    CommentDto mapToDto(Comments comments){
-        CommentDto commentDto=new CommentDto();
+    CommentDto mapToDto(Comments comments) {
+        CommentDto commentDto = new CommentDto();
         commentDto.setId(comments.getId());
         commentDto.setName(comments.getName());
         commentDto.setEmail(comments.getEmail());
         commentDto.setBody(comments.getBody());
         return commentDto;
     }
-    Comments mapToEntity(CommentDto commentDto){
-        Comments comments=new Comments();
+
+    Comments mapToEntity(CommentDto commentDto) {
+        Comments comments = new Comments();
         comments.setId(commentDto.getId());
         comments.setName(commentDto.getName());
         comments.setEmail(commentDto.getEmail());
         comments.setBody(commentDto.getBody());
-        return  comments;
+        return comments;
     }
 }
